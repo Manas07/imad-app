1956f735e340e5eeb60133e6ced4f8dec709d2f2
Manas07 <36154049+Manas07@users.noreply.github.com>
2018-02-19 17:53:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/arctile-one',function(req,res)
+app.get('/article-one',function(req,res)
 {
-    res.send("Arctile one is requised");
+    res.send("Article one is requised");
 });
 
 app.get('/ui/style.css', function (req, res) {
